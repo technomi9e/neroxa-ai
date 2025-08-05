6bf4251e5f2a581c4efc8bc1251b63bb7c8b4d37
technomi9e <technomi9e@gmail.com>
2025-08-05 15:07:44 +0530

    Create App.jsx

---
+++ src/src/src/src/src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-gray-100 dark:bg-gray-900">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
